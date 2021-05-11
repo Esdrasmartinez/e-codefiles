1864d7719cceab6ff39cde750a47b577e0fe5705
Esdras mtz <Esdras_martinez123@outlook.com>
2021-05-11 15:33:29 -0500

    Beta 2.0

---
+++ potencial_energy.py
@@ -0,0 +1,28 @@
+
+import math
+
+class potencial_energy:
+    def __init__(self):
+        pass
+    def potencia_energy(self):
+        print("""
+        CALCULO DE ENERGÍA POTENCIAL
+              Ep = m * g * h 
+        """)
+        masa = float(input("Ingresa la masa: "))
+        altura = float(input("Ingresa la altura relativa: "))
+        if altura == abs(altura):
+            negative_option = input("La altura es positiva?  Y/n...")
+            if negative_option == "Y" or negative_option == "y":
+                altura = altura 
+            elif negative_option == "N" or negative_option == "n":
+                altura = altura * -1
+        Pe = masa * altura * 9.8
+        
+        cache = [masa,altura,Pe]
+        return cache
+
+
+potencial_energy_calculate = potencial_energy()
+PE_values = potencial_energy_calculate.potencia_energy()
+print(f"{PE_values[2]} joules")
